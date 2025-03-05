d416ca0a7c69758b67b72012e25008cdcc156395
Zayon29 <136538073+Zayon29@users.noreply.github.com>
2025-03-05 14:36:51 -0300

    a
    a

--- src/main/java/DataBaseConnection.java
+++ src/main/java/DataBaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DataBaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/meu_banco";
     private static final String USER = "root";
-    private static final String PASSWORD = "zayon";
+    private static final String PASSWORD = "minha_senha";
 
     public static void main(String[] args) {
         try {
