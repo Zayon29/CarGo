a8d7d64f947e2dc99a0bd89d1d763c866c69ee4e
Zayon29 <zayontavares92@mail.com>
2025-03-05 14:14:13 -0300

    Adicionando código Maven ao projeto

---
+++ src/main/java/DataBaseConnection.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/meu_banco";
+    private static final String USER = "root";
+    private static final String PASSWORD = "zayon";
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexão bem-sucedida! 🎉");
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Erro na conexão: " + e.getMessage());
+        }
+    }
+}
